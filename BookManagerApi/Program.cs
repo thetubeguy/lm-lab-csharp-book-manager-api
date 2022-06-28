b065b2d7166c4b491d5af905298febd96aeb7cd6
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-06-28 13:32:01 +0100

    Completed lab without extensions

--- BookManagerApi/Program.cs
+++ BookManagerApi/Program.cs
@@ -8,9 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IBookManagementService, BookManagementService>();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<BookContext>(option =>
+var connectionString = builder.Configuration.GetConnectionString("BookManagerApi");
+if (builder.Environment.EnvironmentName == "Testing")
+{
+    // in test environment use a fresh in-memory DB
+    builder.Services.AddDbContext<BookContext>(option =>
     option.UseInMemoryDatabase("BookDb"));
-
+}
+else
+{
+    // connect to the local MySQL dev database
+    builder.Services.AddDbContext<BookContext>(option =>
+    option.UseMySql(connectionString,
+    ServerVersion.AutoDetect(connectionString)));
+}
 // Configure Swagger/OpenAPI Documentation
 // You can learn more on this link: https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +30,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Testing")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
